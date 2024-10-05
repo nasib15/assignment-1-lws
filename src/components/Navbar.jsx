e6b21470cfe620989e9e0b632e044b61daf8d976
nasib15 <nasibhossain57@gmail.com>
2024-10-05 20:01:34 +0600

    Update: Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,18 @@
 import logo from "../assets/logo/logo.svg";
 
+function NavMiddle({ href, text }) {
+  return (
+    <li className="group relative">
+      <a
+        href={href}
+        className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70"
+      >
+        {text}
+      </a>
+    </li>
+  );
+}
+
 export default function Navbar() {
   return (
     <div className="ud-header absolute left-0 top-0 z-40 flex w-full items-center bg-transparent">
@@ -11,36 +24,10 @@ export default function Navbar() {
             </a>
 
             <ul className="hidden lg:flex ">
-              <li className="group relative">
-                <a
-                  href="#home"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="group relative">
-                <a
-                  href="#about"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10"
-                >
-                  About
-                </a>
-              </li>
-              <li className="group relative">
-                <a
-                  href="#pricing"
-                  className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10"
-                >
-                  Pricing
-                </a>
-              </li>
-
-              <li className="group relative">
-                <a className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-7 lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:ml-10">
-                  Blog
-                </a>
-              </li>
+              <NavMiddle href={"#home"} text={"Home"} />
+              <NavMiddle href={"#about"} text={"About"} />
+              <NavMiddle href={"#pricing"} text={"Pricing"} />
+              <NavMiddle href={"#blog"} text={"Blog"} />
             </ul>
 
             <div className="sm:flex">
