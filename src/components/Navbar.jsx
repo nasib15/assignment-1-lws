20f710ead8f196999f7633ec50efc04500b99f8a
nasib15 <nasibhossain57@gmail.com>
2024-10-05 21:19:11 +0600

    Add: Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import logo from "../assets/logo/logo.svg";
 
-function NavMiddle({ href, text }) {
+function NavMiddle({ href, text, largeDevice = 0, xlDevice = 0 }) {
   return (
     <li className="group relative">
       <a
         href={href}
-        className="ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70"
+        className={`ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 
+           lg:${largeDevice} xl:${xlDevice}`}
       >
         {text}
       </a>
@@ -25,16 +26,31 @@ export default function Navbar() {
 
             <ul className="hidden lg:flex ">
               <NavMiddle href={"#home"} text={"Home"} />
-              <NavMiddle href={"#about"} text={"About"} />
-              <NavMiddle href={"#pricing"} text={"Pricing"} />
-              <NavMiddle href={"#blog"} text={"Blog"} />
+              <NavMiddle
+                href={"#about"}
+                text={"About"}
+                largeDevice={"ml-7"}
+                xlDevice={"ml-10"}
+              />
+              <NavMiddle
+                href={"#pricing"}
+                text={"Pricing"}
+                largeDevice={"ml-7"}
+                xlDevice={"ml-10"}
+              />
+              <NavMiddle
+                href={"#blog"}
+                text={"Blog"}
+                largeDevice={"ml-7"}
+                xlDevice={"ml-10"}
+              />
             </ul>
 
             <div className="sm:flex">
-              <a className="flex-1 loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70">
+              <a className="flex-1 loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70 hover:cursor-pointer">
                 Sign In
               </a>
-              <a className="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+              <a className="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark hover:cursor-pointer">
                 Sign Up
               </a>
             </div>
