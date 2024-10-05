c9b85f122524f39673566f89c16eb9368e4e5319
nasib15 <nasibhossain57@gmail.com>
2024-10-05 19:53:53 +0600

    Add: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/IconSVG.jsx
@@ -0,0 +1,3 @@
+export default function IconSVG({ svg }) {
+  return svg;
+}
