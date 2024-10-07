26c2d7419cc61b9f57c82ed7bd6655d0d067d57f
nasib15 <nasibhossain57@gmail.com>
2024-10-07 11:59:19 +0600

    Fix: here and there

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import logo from "../assets/logo/logo.svg";
 
-function NavMiddle({ href, text, largeDevice = 0, xlDevice = 0 }) {
+function NavMiddle({ href, text, largeDevice = "ml-0", xlDevice = "ml-0" }) {
   return (
     <li className="group relative">
       <a
         href={href}
-        className={`ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 lg:${largeDevice} xl:${xlDevice}`}
+        className={`ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:${largeDevice} lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 lg:text-white lg:group-hover:text-white lg:group-hover:opacity-70 xl:${xlDevice}`}
       >
         {text}
       </a>
